358ec6d356a0956ba91d7468bd2283c741500552
Jeremy Hynes <jhynes@jeremyhynes.com>
2026-01-01 10:12:05 -0400

    Unify header and footer surface colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import HeaderBand from "./components/HeaderBand";
+/* import HeaderBand from "./components/HeaderBand";   */
 
 export const metadata = {
   title: "Coastonia Technology Services",
